1137e442490bbfc8309776232beca9ff94f89913
Jason Kao <jasonkao85@gmail.com>
2018-11-27 17:31:23 -0500

    yat

--- ardino.ino
+++ ardino.ino
@@ -4,8 +4,7 @@
 
 const int ground = GLCD.Bottom - 10;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   // Initialize the GLCD 
   GLCD.Init();
@@ -17,8 +16,17 @@ void setup()
   drawSkeleton();
 }
 
+int dinoSize = 12;
+int dinoX = 0;
+int dinoY = 0;
+void drawDino(int x, int y) {
+  GLCD.FillCircle(dinoX, dinoY, dinoSize, PIXEL_OFF);
+  GLCD.FillCircle(x, y, dinoSize);
+}
+
 void drawSkeleton() {
-  GLCD.FillRect(GLCD.Left, ground, GLCD.Right, GLCD.Bottom);    
+  GLCD.FillRect(GLCD.Left, ground, GLCD.Right, GLCD.Bottom);
+  drawDino(GLCD.Left + dinoSize + 6, ground - dinoSize);
 }
 
 int blocks[10][3];
@@ -26,8 +34,8 @@ int numBlocks = 0;
 long placeBlockAt = 200;
 
 void placeBlock() {
-  int height = random(4, 8);
-  int width = random(4, 6);
+  int height = random(14, 20);
+  int width = random(8, 12);
   // blocks are stored as { x, y, width }
   blocks[numBlocks][0] = GLCD.Width;
   blocks[numBlocks][1] = ground - height;
@@ -47,11 +55,31 @@ void updateBlocks() {
   }
 }
 
+int jumpHeight = 20;
+boolean jumpDino() {
+  if (dinoY < jumpHeight) {
+    dinoY++;
+  } else {
+    dinoY--;
+  }
+  return dinoY >= ground; //VELEAADSBBOOLEANB
+}
+
+boolean isJumping = false;
 void loop() {
   if (millis() >= placeBlockAt) {
     placeBlock();
-    placeBlockAt += random(400, 600);
+    placeBlockAt += random(2500, 5000);
   }
   updateBlocks();
+  if (!isJumping && random(0, 4000) == 1) { // jump randomly
+    isJumping = true;
+  }
+  if (isJumping) {
+    jumpDino();
+    if (dinoY <= ground) {
+      isJumping = false;
+    }
+  }
   delay(100);
 }
